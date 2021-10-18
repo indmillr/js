a8ded2a93fc28c0cfcbb2f0415b7cab9f6dd730d
Ian Miller <theiandmiller@gmail.com>
2021-10-17 22:07:13 -0500

    addEventListener to show Next, Previous, and Random review/person

--- reviewCarousel/app.js
+++ reviewCarousel/app.js
@@ -27,5 +27,56 @@ const reviews = [
       job: "the boss",
       img: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg",
       text: "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ",
-    },
+     },
   ];
+
+// select items
+const img = document.getElementById('person-img');
+const author = document.getElementById('author');
+const job = document.getElementById('job');
+const info = document.getElementById('info');
+
+const prevBtn = document.querySelector('.prev-btn');
+const nextBtn = document.querySelector('.next-btn');
+const randomBtn = document.querySelector('.random-btn');
+
+// set starting item
+let currentItem = 0;
+
+// load initial item
+window.addEventListener('DOMContentLoaded', function() {
+  showPerson(currentItem);
+});
+
+// show person based on item
+function showPerson(person) {
+  const item = reviews[person];
+  img.src = item.img;
+  author.textContent = item.name;
+  job.textContent = item.job;
+  info.textContent = item.text;
+}
+
+// show next review/person
+nextBtn.addEventListener('click', function (){
+  currentItem++;
+  if(currentItem > reviews.length - 1){ // Reset currentItem to 0 once end of array is reached
+    currentItem = 0;
+  } 
+  showPerson(currentItem);
+});
+
+// show previous review/person
+prevBtn.addEventListener('click', function() {
+  currentItem--;
+  if(currentItem < 0) {
+    currentItem = reviews.length - 1;
+  }
+  showPerson(currentItem);
+});
+
+// show random review/person
+randomBtn.addEventListener('click', function() {
+  currentItem = Math.floor(Math.random() * reviews.length);
+  showPerson(currentItem);
+});
