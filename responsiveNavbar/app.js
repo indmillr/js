2d478e930578631c30277db6ae130711bd1491c4
Ian Miller <theiandmiller@gmail.com>
2021-10-18 20:57:57 -0500

    toggle show-links class

---
+++ responsiveNavbar/app.js
@@ -0,0 +1,23 @@
+// classList --> shows/gets all classes
+// contains --> checks classList for specific class
+// add method --> ADD class
+// remove method --> REMOVE class
+// toggle method --> TOGGLE class
+
+const navToggle = document.querySelector(".nav-toggle");
+const links = document.querySelector(".links");
+
+navToggle.addEventListener('click', function(){
+    // console.log(links.classList);
+    // console.log(links.classList.contains('random')); <-- FALSE
+    // console.log(links.classList.contains('links')); <-- TRUE
+
+    // if(links.classList.contains('show-links')){
+    //     links.classList.remove('show-links')
+    // }
+    // else {
+    //     links.classList.add('show-links');
+    // }
+
+    links.classList.toggle('show-links'); // This LOC replaces Lines 11-20
+});
