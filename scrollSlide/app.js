fbd09f4fb805b0054de1cf7e1f8ca208389982d5
Ian Miller <theiandmiller@gmail.com>
2021-11-04 21:02:21 -0500

    initial comments on app.js to explain sections and actions

---
+++ scrollSlide/app.js
@@ -0,0 +1,21 @@
+/* Element.getBoundingClientRect() method returns the size of
+an element and its position relative to the viewport. */
+
+/* pageYOffset is a read-only window property that returns
+the number of pixels the document has been scrolled vertically. */
+
+/* slice extracts a section of a string without modifying the
+original string */
+
+/* offsetTop - A Number, representing the top position of the
+element, in pixels. */
+
+// ***** SET THE DATE ***** 
+
+// ***** CLOSE LINKS ***** 
+
+// ***** FIXED NAVBAR ***** 
+
+// ***** SMOOTH SCROLL ***** 
+
+// Select Links
