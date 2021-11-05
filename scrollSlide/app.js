de431ecc1a890f514e8295af68dfff75580ebb0d
Ian Miller <theiandmiller@gmail.com>
2021-11-04 21:36:17 -0500

    fixed navbar and added 'return to top' button show/hide

--- scrollSlide/app.js
+++ scrollSlide/app.js
@@ -20,10 +20,32 @@ const linksContainer = document.querySelector('.links-container');
 const links = document.querySelector('.links');
 
 navToggle.addEventListener('click', function() {
-  linksContainer.classList.toggle('show-links');
+  // linksContainer.classList.toggle('show-links');
+  const containerHeight = linksContainer.getBoundingClientRect().height;
+  const linksHeight = links.getBoundingClientRect().height;
+
+  if(containerHeight === 0) {
+    linksContainer.style.height = `${linksHeight}px`;
+  }
+  else {
+    linksContainer.style.height = 0;
+  }
 });
 
+const navbar = document.getElementById('nav');
+const topLink = document.querySelector('.top-link');
 // ***** FIXED NAVBAR ***** 
+window.addEventListener('scroll', function() {
+  const scrollHeight = window.pageYOffset;
+  const navHeight = navbar.getBoundingClientRect().height;
+
+  if (scrollHeight > navHeight) {
+    navbar.classList.add('fixed-nav');
+  } else {
+    navbar.classList.remove('fixed-navbar');
+  }
+
+});
 
 // ***** SMOOTH SCROLL ***** 
 
